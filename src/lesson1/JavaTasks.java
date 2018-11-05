13511186d93c665a7530f0384080787782075dec
Vasiliy_Macsyom <seadlow147@gmail.com>
2018-11-05 10:54:14 +0300

    Kotoed only tests

--- src/lesson1/JavaTasks.java
+++ src/lesson1/JavaTasks.java
@@ -3,38 +3,36 @@ package lesson1;
 import kotlin.NotImplementedError;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.IllegalFormatException;
+import java.util.*;
 
 @SuppressWarnings("unused")
 public class JavaTasks {
     /**
      * Сортировка времён
-     *
+     * <p>
      * Простая
      * (Модифицированная задача с сайта acmp.ru)
-     *
+     * <p>
      * Во входном файле с именем inputName содержатся моменты времени в формате ЧЧ:ММ:СС,
      * каждый на отдельной строке. Пример:
-     *
+     * <p>
      * 13:15:19
      * 07:26:57
      * 10:00:03
      * 19:56:14
      * 13:15:19
      * 00:40:31
-     *
+     * <p>
      * Отсортировать моменты времени по возрастанию и вывести их в выходной файл с именем outputName,
      * сохраняя формат ЧЧ:ММ:СС. Одинаковые моменты времени выводить друг за другом. Пример:
-     *
+     * <p>
      * 00:40:31
      * 07:26:57
      * 10:00:03
      * 13:15:19
      * 13:15:19
      * 19:56:14
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
     static public void sortTimes(String inputName, String outputName) {
@@ -43,28 +41,28 @@ public class JavaTasks {
 
     /**
      * Сортировка адресов
-     *
+     * <p>
      * Средняя
-     *
+     * <p>
      * Во входном файле с именем inputName содержатся фамилии и имена жителей города с указанием улицы и номера дома,
      * где они прописаны. Пример:
-     *
+     * <p>
      * Петров Иван - Железнодорожная 3
      * Сидоров Петр - Садовая 5
      * Иванов Алексей - Железнодорожная 7
      * Сидорова Мария - Садовая 5
      * Иванов Михаил - Железнодорожная 7
-     *
+     * <p>
      * Людей в городе может быть до миллиона.
-     *
+     * <p>
      * Вывести записи в выходной файл outputName,
      * упорядоченными по названию улицы (по алфавиту) и номеру дома (по возрастанию).
      * Людей, живущих в одном доме, выводить через запятую по алфавиту (вначале по фамилии, потом по имени). Пример:
-     *
+     * <p>
      * Железнодорожная 3 - Петров Иван
      * Железнодорожная 7 - Иванов Алексей, Иванов Михаил
      * Садовая 5 - Сидоров Петр, Сидорова Мария
-     *
+     * <p>
      * В случае обнаружения неверного формата файла бросить любое исключение.
      */
     static public void sortAddresses(String inputName, String outputName) {
@@ -73,14 +71,14 @@ public class JavaTasks {
 
     /**
      * Сортировка температур
-     *
+     * <p>
      * Средняя
      * (Модифицированная задача с сайта acmp.ru)
-     *
+     * <p>
      * Во входном файле заданы температуры различных участков абстрактной планеты с точностью до десятых градуса.
      * Температуры могут изменяться в диапазоне от -273.0 до +500.0.
      * Например:
-     *
+     * <p>
      * 24.7
      * -12.6
      * 121.3
@@ -88,11 +86,11 @@ public class JavaTasks {
      * 99.5
      * -12.6
      * 11.0
-     *
+     * <p>
      * Количество строк в файле может достигать ста миллионов.
      * Вывести строки в выходной файл, отсортировав их по возрастанию температуры.
      * Повторяющиеся строки сохранить. Например:
-     *
+     * <p>
      * -98.4
      * -12.6
      * -12.6
@@ -113,7 +111,7 @@ public class JavaTasks {
         }
         Collections.sort(answer);
         BufferedWriter writer = new BufferedWriter(new FileWriter(inputName));
-        for (double elements : answer){
+        for (double elements : answer) {
             writer.write(String.valueOf(elements));
             writer.newLine();
         }
@@ -122,12 +120,12 @@ public class JavaTasks {
 
     /**
      * Сортировка последовательности
-     *
+     * <p>
      * Средняя
      * (Задача взята с сайта acmp.ru)
-     *
+     * <p>
      * В файле задана последовательность из n целых положительных чисел, каждое в своей строке, например:
-     *
+     * <p>
      * 1
      * 2
      * 3
@@ -135,12 +133,12 @@ public class JavaTasks {
      * 3
      * 1
      * 2
-     *
+     * <p>
      * Необходимо найти число, которое встречается в этой последовательности наибольшее количество раз,
      * а если таких чисел несколько, то найти минимальное из них,
      * и после этого переместить все такие числа в конец заданной последовательности.
      * Порядок расположения остальных чисел должен остаться без изменения.
-     *
+     * <p>
      * 1
      * 3
      * 3
@@ -149,25 +147,62 @@ public class JavaTasks {
      * 2
      * 2
      */
-    static public void sortSequence(String inputName, String outputName) {
-        throw new NotImplementedError();
+    static public void sortSequence(String inputName, String outputName) throws IOException,
+            IllegalFormatException {
+        ArrayList<Integer> listOfNumbers = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        int maxValue = Integer.MAX_VALUE;
+        int value, count = 0;
+        String textLine;
+        BufferedReader reader = new BufferedReader(new FileReader(inputName));
+        {
+            while ((textLine = reader.readLine()) != null) {
+                int key = Integer.parseInt(textLine);
+                listOfNumbers.add(key);
+                if (map.containsKey(key)) {
+                    value = map.get(key);
+                    map.put(key, value + 1);
+                    if (value > count || (value == count && key < maxValue)) {
+                        count = value;
+                        maxValue = key;
+                    } else {
+                        map.put(key, 1);
+                        if (value == count && key < maxValue) {
+                            maxValue = key;
+                        }
+                    }
+                }
+                BufferedWriter writer = new BufferedWriter(new FileWriter(inputName));
+                for (int elements : listOfNumbers) {
+                    if (elements != maxValue) {
+                        writer.write(String.valueOf(elements));
+                        writer.newLine();
+                    }
+                }
+                for (int i = 0; i < count + 1; i++){
+                    writer.write(String.valueOf(maxValue));
+                    writer.newLine();
+                }
+                writer.close();
+            }
+        }
     }
 
-    /**
-     * Соединить два отсортированных массива в один
-     *
-     * Простая
-     *
-     * Задан отсортированный массив first и второй массив second,
-     * первые first.size ячеек которого содержат null, а остальные ячейки также отсортированы.
-     * Соединить оба массива в массиве second так, чтобы он оказался отсортирован. Пример:
-     *
-     * first = [4 9 15 20 28]
-     * second = [null null null null null 1 3 9 13 18 23]
-     *
-     * Результат: second = [1 3 4 9 9 13 15 20 23 28]
-     */
-    static <T extends Comparable<T>> void mergeArrays(T[] first, T[] second) {
-        // Fake solution
+        /**
+         * Соединить два отсортированных массива в один
+         * <p>
+         * Простая
+         * <p>
+         * Задан отсортированный массив first и второй массив second,
+         * первые first.size ячеек которого содержат null, а остальные ячейки также отсортированы.
+         * Соединить оба массива в массиве second так, чтобы он оказался отсортирован. Пример:
+         * <p>
+         * first = [4 9 15 20 28]
+         * second = [null null null null null 1 3 9 13 18 23]
+         * <p>
+         * Результат: second = [1 3 4 9 9 13 15 20 23 28]
+         */
+        static <T extends Comparable<T>>void mergeArrays (T[]first, T[]second){
+            // Fake solution
+        }
     }
-}
