2110a63a409b1d0b38659cbde83d496f17e4ae65
Vasiliy_Macsyom <seadlow147@gmail.com>
2018-11-19 12:55:39 +0300

    Test

--- test/lesson3/AbstractHeadTailTest.kt
+++ test/lesson3/AbstractHeadTailTest.kt
@@ -8,55 +8,57 @@ abstract class AbstractHeadTailTest {
 
     protected fun fillTree(empty: SortedSet<Int>) {
         this.tree = empty
-        //В произвольном порядке добавим числа от 1 до 10
-        tree.add(5)
-        tree.add(1)
-        tree.add(2)
-        tree.add(7)
-        tree.add(9)
-        tree.add(10)
-        tree.add(8)
-        tree.add(4)
-        tree.add(3)
-        tree.add(6)
+        var k = 0
+        while (k < 60){
+            k++
+            tree.add(k)
+        }
     }
 
 
     protected fun doHeadSetTest() {
-        var set: SortedSet<Int> = tree.headSet(5)
+        var set: SortedSet<Int> = tree.headSet(9)
         assertEquals(true, set.contains(1))
         assertEquals(true, set.contains(2))
         assertEquals(true, set.contains(3))
         assertEquals(true, set.contains(4))
-        assertEquals(false, set.contains(5))
-        assertEquals(false, set.contains(6))
-        assertEquals(false, set.contains(7))
-        assertEquals(false, set.contains(8))
+        assertEquals(true, set.contains(5))
+        assertEquals(true, set.contains(6))
+        assertEquals(true, set.contains(7))
+        assertEquals(true, set.contains(8))
         assertEquals(false, set.contains(9))
+        assertEquals(false, set.contains(21))
+        assertEquals(false, set.contains(14))
+        assertEquals(false, set.contains(17))
+        assertEquals(false, set.contains(22))
         assertEquals(false, set.contains(10))
 
 
         set = tree.headSet(127)
-        for (i in 1..10)
+        for (i in 1..55)
             assertEquals(true, set.contains(i))
 
     }
 
     protected fun doTailSetTest() {
-        var set: SortedSet<Int> = tree.tailSet(5)
+        var set: SortedSet<Int> = tree.tailSet(7)
         assertEquals(false, set.contains(1))
         assertEquals(false, set.contains(2))
         assertEquals(false, set.contains(3))
         assertEquals(false, set.contains(4))
-        assertEquals(true, set.contains(5))
-        assertEquals(true, set.contains(6))
+        assertEquals(false, set.contains(5))
+        assertEquals(false, set.contains(6))
         assertEquals(true, set.contains(7))
         assertEquals(true, set.contains(8))
         assertEquals(true, set.contains(9))
         assertEquals(true, set.contains(10))
+        assertEquals(true, set.contains(44))
+        assertEquals(true, set.contains(23))
+        assertEquals(true, set.contains(54))
+        assertEquals(true, set.contains(10))
 
         set = tree.tailSet(-128)
-        for (i in 1..10)
+        for (i in 1..55)
             assertEquals(true, set.contains(i))
 
     }
